7eeb648356689bc4c06d873b4356ec50434b5ece
Niapoll <kolya21082001@gmail.com>
2022-10-19 14:20:46 +0300

    Add compositions in Task 1 and rewrite sumOfFirst function

--- Task 1/task_1.hs
+++ Task 1/task_1.hs
@@ -4,50 +4,45 @@ import Data.Char(isUpper, isLower)
 
 -- 1
 --
-discriminant :: Num a => a -> a -> a -> a
-discriminant a b c = b ^ 2 - 4 * a * c
-
-solveQuadraticEquation :: (Floating a, Real a) => a -> a -> a -> (a, Maybe a)
-solveQuadraticEquation 0 0 _ = error "Infinity solutions count."
+solveQuadraticEquation :: (Floating a, Eq a) => a -> a -> a -> (a, Maybe a)
+solveQuadraticEquation 0 0 0 = error "Infinity solutions count."
+solveQuadraticEquation 0 0 _ = error "No solutions."
 solveQuadraticEquation 0 b c = (-c / b, Nothing)
 solveQuadraticEquation a b c = ((b + sqrt d) / 2 * a, Just ((b - sqrt d) / 2 * a))
     where
         d = discriminant a b c
+        discriminant a b c = b ^ 2 - 4 * a * c
 
 -- 2
 --
-digits :: Integral a => a -> [a]
-digits 0 = []
-digits number = digits (div number 10) ++ [mod number 10]
-
 sumAndCount :: Integral a => a -> (a, Int)
 sumAndCount number = (sum d, length d)
     where
-        d = digits (abs number)
+        d = digits . abs $ number
+        digits 0 = []
+        digits number = number `mod` 10 : digits (number `div` 10)
 
 -- 3
 --
 removeCapitalizedWords :: [Char] -> String
-removeCapitalizedWords text = unwords (filter cond w)
+removeCapitalizedWords text = unwords . filter cond . words $ text
     where
         cond = all isLower
-        w = words text
 
 -- 4
 --
-sumOfFirst :: (Fractional a, Eq a, Enum a) => a -> a
+sumOfFirst :: Int -> Double
 sumOfFirst 0 = 0
-sumOfFirst count = sum (map mutator [1..count])
+sumOfFirst count = sum . take count . map mutator $ [1..]
     where
-        mutator x = x / (count ^ 2)
+        mutator x = 1 / (x ^ 2)
 
 -- 5
 --
 deleteEach :: Integral a => a -> [b] -> [b]
-deleteEach index elements = map snd (filter cond pairs)
+deleteEach index elements = map snd . filter cond $ zip [1..] elements
     where
-        cond element = (mod (fst element) index) /= 0
-        pairs = zip [1..] elements
+        cond element = fst element `mod` index /= 0
 
 -- 6
 --
@@ -55,48 +50,46 @@ takeWhileIncreasing :: (Integral a) => [a] -> ([a], [a])
 takeWhileIncreasing [] = ([], [])
 takeWhileIncreasing elements = (increasing, notIncreasing)
     where
-        increasing = if length elements > 1
-            then
-                findIncreasing elements
-            else
-                elements
+        increasing
+            | length elements > 1 = findIncreasing elements
+            | otherwise = elements
         notIncreasing = drop (length increasing) elements
-        findIncreasing (x:tail) = if x <= head tail
-            then
-                if length tail == 1
+        findIncreasing (x:tail)
+            | x <= head tail = if length tail == 1
                     then
                         x:tail
                     else
                         x:findIncreasing tail
-            else
-                [x]
+            | otherwise = [x]
 
 increasingSubsequence :: Integral a => [a] -> [[a]]
 increasingSubsequence [] = []
-increasingSubsequence elements = if length tail > 0
-    then
-        increasing:increasingSubsequence tail
-    else
-        [increasing]
+increasingSubsequence elements
+    | length tail > 0 = increasing:increasingSubsequence tail
+    | otherwise = [increasing]
     where
         increasing = fst subsets
         tail = snd subsets
         subsets = takeWhileIncreasing elements
 
 maxIncreasingSubsequence :: Integral a => [a] -> [[a]]
-maxIncreasingSubsequence elements = filter cond sequences
+maxIncreasingSubsequence elements = map fst . filter cond $ pairSequenceLength
     where
-        cond element = length element == maxElementsCount
-        maxElementsCount = maximum (map length sequences)
+        cond x = snd x == maxElementsCount
+        maxElementsCount = maximum . map snd $ pairSequenceLength
+        pairSequenceLength = zip sequences . map length $ sequences
         sequences = increasingSubsequence elements
 
 -- 7
 --
 delete :: Eq a => a -> [a] -> [a]
-delete value elements = firstPart ++ secondPart
+delete value elements = deleteValue elements
     where
-        secondPart = drop (length firstPart + 1) elements
-        firstPart = takeWhile (/= value) elements
+        deleteValue [] = []
+        deleteValue (x:tail)
+            | x == value = tail
+            | otherwise = x:deleteValue tail
+
 
 permutations :: Eq a => [a] -> [[a]]
 permutations [] = [[]]
